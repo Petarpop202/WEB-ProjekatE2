7e4e0cbce05aeed516630372efc19bf1150f8275
Petar Popovic <93181074+Petarpop202@users.noreply.github.com>
2022-06-18 15:21:01 +0200

    Registration PopUp

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -23,3 +23,58 @@
 			let n = document.getElementById("Objekti");
 			n.innerHTML = i;
 		}
+
+		$(window).ready(function() {
+			$("#register").click(function (event) {
+				event.preventDefault();
+		
+				let username = $('input[name="username"]').val();
+				let password = $('input[name="password"]').val();
+				let name = $('input[name="name"]').val();
+				let surname = $('input[name="surname"]').val();
+				let gender = $('select[name="gender"]').find(":selected").val();
+				let date = $('input#datum').val();
+		
+						
+					$.post({
+						url: "rest/registration",
+						contentType: "application/json",
+						data: JSON.stringify({
+							Username: username,
+							Password: password,
+							Name: name,
+							Surname: surname,
+							Gender: gender,
+							Date: date,
+						}),
+						success: function(odgovor) {
+							alert("Korisnik " + odgovor.Username + " je registrovan!")
+							
+							let urlLogin = "../rest/prijava?username=" + odgovor.korisnickoIme + "&password=" + odgovor.lozinka
+							/*
+							$.post({
+								   url: urlLogin,
+								contentType: "application/json",
+								success: function(odgovor) {
+									sessionStorage.setItem("jwt", odgovor.jwt);
+									window.location.assign("http://localhost:8088/wp-projekat/html/sopstveni_profil.html");
+								},
+								error: function(odgovor) {
+										alert(odgovor.responseText);
+								}
+							});
+							*/
+		
+							
+						},
+						error: function(odgovor) {
+							if (odgovor.status == 400) {
+								alert("Korisnik sa zadatim korisnickim imenom vec postoji!");
+							}else {
+								alert("Greska pri registraciji!");
+							}
+						}
+					});
+				
+			});
+		})
