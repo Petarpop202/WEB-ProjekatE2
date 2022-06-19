2b8f8ece25634e965ba48ecd6973a8c636cafb6c
Petar Popovic <93181074+Petarpop202@users.noreply.github.com>
2022-06-19 12:16:16 +0200

    Login merge

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -1,6 +1,7 @@
 /**
  * 
  */
+
 		$(document).ready(function () {
 						$.ajax({
 							url : "rest/sports/",
@@ -21,18 +22,61 @@
 			n.innerHTML = i;
 		}
 
-		$(window).ready(function() {
-			$("#register").click(function (event) {
-				event.preventDefault();
-		
-				let username = $('input[name="username"]').val();
+		function validateCheck(document){
+			let username = $('input[name="username"]').val();
 				let password = $('input[name="password"]').val();
 				let name = $('input[name="name"]').val();
 				let surname = $('input[name="surname"]').val();
 				let gender = $('select[name="gender"]').find(":selected").val();
 				let date = $('input#datum').val();
-		
-						
+				let validate = false;
+
+				var regName = new RegExp('([A-Z]{1})([a-z]+)$');
+				var regUser = new RegExp('[a-zA-Z0-9]+$');
+				if(!regName.test(name)){
+					document.getElementById("eri").removeAttribute("hidden");
+				}else{
+					document.getElementById("eri").setAttribute("hidden","true");
+					validate = true;
+				}
+				if(!regName.test(surname)){
+					document.getElementById("erp").removeAttribute("hidden");
+					validate = false;
+				}
+				else{
+					document.getElementById("erp").setAttribute("hidden","true");
+					if(validate)
+					validate = true;
+				}
+				if(date == ""){
+					document.getElementById("erd").removeAttribute("hidden");
+					validate = false;
+				}
+				else{
+					document.getElementById("erd").setAttribute("hidden","true");
+					if(validate)
+					validate = true;
+				}
+				if(!regUser.test(username)){
+					document.getElementById("erk").removeAttribute("hidden");
+					validate = false;
+				}
+				else{
+					document.getElementById("erk").setAttribute("hidden","true");
+					if(validate)
+					validate = true;
+				}
+				if(!regUser.test(password)){
+					document.getElementById("erl").removeAttribute("hidden");
+					validate = false;
+				}
+				else{
+					document.getElementById("erl").setAttribute("hidden","true");
+					if(validate)
+					validate = true;
+				}
+
+				if(validate){
 					$.post({
 						url: "rest/registration",
 						contentType: "application/json",
@@ -66,18 +110,66 @@
 						},
 						error: function(odgovor) {
 							if (odgovor.status == 400) {
+								document.getElementById("errorUser").removeAttribute("hidden");
+							}else {
+								document.getElementById("errorUser").setAttribute("hidden","true");
+								alert("Greska pri registraciji!");
+							}
+						}
+					});
+				}
+
+		}
+		$(window).ready(function() {
+			$("#register").click(function (event) {
+				event.preventDefault();
+				validateCheck(document);
+				/*	
+					$.post({
+						url: "rest/registration",
+						contentType: "application/json",
+						data: JSON.stringify({
+							username: username,
+							password: password,
+							name: name,
+							surname: surname,
+							gender: gender,
+							date: date,
+						}),
+						success: function(odgovor) {
+							alert("Korisnik " + odgovor.username + " je registrovan!")
+							
+							//let urlLogin = "../rest/prijava?username=" + odgovor.korisnickoIme + "&password=" + odgovor.lozinka
+							/*
+							$.post({
+								   url: urlLogin,
+								contentType: "application/json",
+								success: function(odgovor) {
+									sessionStorage.setItem("jwt", odgovor.jwt);
+									window.location.assign("http://localhost:8088/wp-projekat/html/sopstveni_profil.html");
+								},
+								error: function(odgovor) {
+										alert(odgovor.responseText);
+								}
+							});
+							
+		
+							
+						},
+						error: function(odgovor) {
+							if (odgovor.status == 400) {
 								alert("Korisnik sa zadatim korisnickim imenom vec postoji!");
 							}else {
 								alert("Greska pri registraciji!");
 							}
 						}
 					});
+					*/
 				
 			});
 		})
 
 		$(document).ready(function() {
-
 			$("#loginBtn").click(function (event) {
 				event.preventDefault();
 		
@@ -88,11 +180,11 @@
 						url: "rest/login?username=" + username + "&password=" + password,
 						contentType: "application/json",
 						success: function(odgovor) {
-							//sessionStorage.setItem("jwt", odgovor.jwt);
-							alert("Uspesna prijave");
+							alert("Uspesna prijava");
 						},
 						error: function(odgovor) {
-							alert("Greska prijave");
+							document.getElementById("er").removeAttribute("hidden");
+							return;
 						}
 					});			
 					
