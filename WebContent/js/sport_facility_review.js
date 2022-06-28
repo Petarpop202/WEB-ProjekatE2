f665d6d8795216dce926d47ab690450a380bcad0
Petar Popovic <93181074+Petarpop202@users.noreply.github.com>
2022-06-28 17:58:41 +0200

    fix front

--- WebContent/js/sport_facility_review.js
+++ WebContent/js/sport_facility_review.js
@@ -14,10 +14,19 @@
         contentType:"application/json",
         dataType:"json",
         success: function(objekat){
-            let i = objekat;
+            reviewObj(objekat);
         }
     })
 
     }
 
  });
+
+ function reviewObj(objekat){
+    let n = document.getElementById("main");
+    n.innerHTML = '<div class="container pt-5 pb-5"><h1 class="font-weight-bold text-dark">'+objekat.name+'</h1><h3 class="subhead fs-4 mb-0 text-white-50">'+objekat.location.address+'</h3><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><h5 class="review-count">'+objekat.rate+'</h5></div></div>';
+    n.style.background = "rgb(27,27,27) url('../img/"+objekat.picture+".jpg')";
+    n.style.backgroundSize = "cover";
+    
+}
+
