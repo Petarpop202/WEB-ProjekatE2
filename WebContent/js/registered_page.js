7c09ab1fe4f4089a0c9eb3ab8e0f34a348b15414
davidmijailovic <mijailovicd00@gmail.com>
2022-07-10 17:46:12 +0200

    filtriranje

--- WebContent/js/registered_page.js
+++ WebContent/js/registered_page.js
@@ -107,6 +107,7 @@ $("#sortBtn").click(function(event){
             });
     event.preventDefault();
 });
+
 })
 
 function al(i){
@@ -120,6 +121,38 @@ function al(i){
             window.location.reload();
         },
         error: function(odgovor) {
+        }
+    });
+}
+
+function filtriraj(name){
+
+    $.get({
+        url: "../rest/info/filterUsers?name=" + name,
+        contentType:"application/json",
+        dataType:"json", 
+        success:function(korisnici){
+            let i = 1;
+            let h = '<tr><th>#</th><th>Ime</th><th>Prezime</th><th>Korisnicko ime</th><th>Datum rodjenja</th><th>Aktivan</th><th>Uloga</th><th>Ukloni</th></tr>';
+            for(let s of korisnici){
+                 if(s.gender)
+                     p = "Musko";
+                 else p = "Zensko"
+                 if(s.role == "CUSTOMER")
+                     ul = "Kupac";
+                 else if(s.role == "ADMIN")
+                     ul = "Administrator";
+                 else if(s.role == "MANAGER")
+                     ul = "Menadzer";
+                 else ul = "Trener";
+                 h += '<tr><td>'+i+'</td><td>'+s.name+'</td><td>'+s.surname+'</td><td>'+s.username+'</td><td>'+s.date+'</td><td>'+p+'</td><td>'+ul+'</td><td><button onclick="al(`'+s.username+'`)" class="btn btn-primary">Izbrisi</button></td></tr>';
+                 i++;
+            }
+            let u = document.getElementById("tabela");
+            u.innerHTML = h;
+        },
+        error:function(){
+            alert("Greska!");
         }
     });
 }
